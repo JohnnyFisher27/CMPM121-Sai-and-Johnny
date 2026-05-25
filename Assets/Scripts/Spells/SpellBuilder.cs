6470287ec1ed98ebdfe3896ecbacd6c0bf9878e8
Sai <saahamid@ucsc.edu>
2026-05-25 12:00:25 -0700

    successfully divorced spells from modifiers

--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -10,14 +10,20 @@ public class SpellBuilder
 
     public Spell Build(SpellCaster owner)
     {
-        Debug.Log("SpellCaster speaking !!");
         string jsonString1 = File.ReadAllText(Application.dataPath + "/Resources/spells.json");
+        string jsonString2 = File.ReadAllText(Application.dataPath + "/Resources/modifiers.json");
         JObject root = JObject.Parse(jsonString1);
+        JObject root2 = JObject.Parse(jsonString2);
         foreach (var entry in root)
         {
             Debug.Log(entry.Key);
         }
-       
+        foreach (var entry in root2)
+        {
+            Debug.Log(entry.Key);
+        }
+        Debug.Log("SpellCaster speaking !!");
+
         return new Spell(owner);
     }
 
